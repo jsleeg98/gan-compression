9198a3d3e3984d0631543af0ed091713d853b86b
Lee Donghyeon <jsleeg7279@naver.com>
2024-03-20 22:34:03 +0900

    Fix model path

--- scripts/cycle_gan/horse2zebra/train_supernet.sh
+++ scripts/cycle_gan/horse2zebra/train_supernet.sh
@@ -5,9 +5,8 @@ python train_supernet.py --dataroot database/horse2zebra \
   --log_dir logs/cycle_gan/horse2zebra/supernet \
   --gan_mode lsgan \
   --student_ngf 32 --ndf 64 \
-  --restore_teacher_G_path logs/cycle_gan/horse2zebra/mobile/checkpoints/latest_net_G_A.pth \
-  --restore_student_G_path logs/cycle_gan/horse2zebra/distill/checkpoints/latest_net_G.pth \
-  --restore_D_path logs/cycle_gan/horse2zebra/distill/checkpoints/latest_net_D.pth \
+  --restore_teacher_G_path ./pretrained/cycle_gan/horse2zebra/mobile/latest_net_G.pth \
+  --restore_D_path ./pretrained/cycle_gan/horse2zebra/mobile/latest_net_D.pth \
   --real_stat_path real_stat/horse2zebra_B.npz \
   --lambda_recon 10 --lambda_distill 0.01 \
   --nepochs 200 --nepochs_decay 200 \
@@ -15,3 +14,4 @@ python train_supernet.py --dataroot database/horse2zebra \
   --config_set channels-32 \
   --metaA_path datasets/metas/horse2zebra/train1A.meta \
   --metaB_path datasets/metas/horse2zebra/train1B.meta
+#  --restore_student_G_path logs/cycle_gan/horse2zebra/distill/checkpoints/latest_net_G.pth \
