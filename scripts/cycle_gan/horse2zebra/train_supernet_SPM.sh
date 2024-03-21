bfbb95fe7625098d527029b1287030c3d70021ac
Lee Donghyeon <jsleeg7279@naver.com>
2024-03-21 10:48:30 +0900

    Add train_supernet_SPM.sh

---
+++ scripts/cycle_gan/horse2zebra/train_supernet_SPM.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+python train_supernet.py --dataroot database/horse2zebra \
+  --dataset_mode unaligned \
+  --supernet resnet \
+  --student_netG super_mobile_resnet_9blocks_SPM \
+  --log_dir logs/cycle_gan/horse2zebra/supernet_SPM_test \
+  --gan_mode lsgan \
+  --student_ngf 32 --ndf 64 \
+  --restore_teacher_G_path ./pretrained/cycle_gan/horse2zebra/mobile/latest_net_G.pth \
+  --restore_D_path ./pretrained/cycle_gan/horse2zebra/mobile/latest_net_D.pth \
+  --restore_student_G_path logs/cycle_gan/horse2zebra/supernet_baseline/checkpoints/latest_net_G.pth \
+  --real_stat_path real_stat/horse2zebra_B.npz \
+  --lambda_recon 10 --lambda_distill 0.01 \
+  --nepochs 400 --nepochs_decay 200 \
+  --save_epoch_freq 20 \
+  --config_set channels-32 \
+  --metaA_path datasets/metas/horse2zebra/train1A.meta \
+  --metaB_path datasets/metas/horse2zebra/train1B.meta
+#  --restore_student_G_path logs/cycle_gan/horse2zebra/distill/checkpoints/latest_net_G.pth \
